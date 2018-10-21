e975ba66f2b257ea18b1da8aadbbe3bcc1723a62
WillNess210 <WillNess210@gmail.com>
2018-10-21 10:58:15 -0600

    Recipe upload template finished

--- js/main.js
+++ js/main.js
@@ -34,3 +34,56 @@ function getAllUrlParams(url) {
 
   return obj;
 }
+var numIngredients = 1;
+function addIngredientField(){
+	numIngredients++;;
+	var container = document.getElementById("ingredients");
+	var hr = document.createElement("hr");
+	container.appendChild(hr);
+	var row = document.createElement("div");
+	row.className = "row addform";
+	var col1 = document.createElement("div");
+	col1.className = "col";
+	var inp1 = document.createElement("input");
+	inp1.type = "text";
+	inp1.className = "form-control";
+	inp1.placeholder = "Ingredient " + numIngredients + " Name";
+	inp1.id = "inp" + numIngredients + "name";
+	col1.appendChild(inp1);
+	var col2 = document.createElement("div");
+	col2.className = "col";
+	var inp2 = document.createElement("input");
+	inp2.type = "text";
+	inp2.className = "form-control";
+	inp2.placeholder = "Ingredient Amount (ex: 1 cup, 8 handfuls, etc..)";
+	inp2.id = "inp" + numIngredients + "amt";
+	col2.appendChild(inp2);
+	row.appendChild(col1);
+	row.appendChild(col2);
+	container.appendChild(row);
+}
+
+var numSteps = 1;
+function addStepField(){
+	numSteps++;
+	var container = document.getElementById("steps");
+	var hr = document.createElement("hr");
+	container.appendChild(hr);
+	var row = document.createElement("div");
+	row.className = "form-group row";
+	var label = document.createElement("label");
+	label.for = "step" + numSteps;
+	label.className = "col-sm-1 col-form-label";
+	label.textContent = "Step " + numSteps;
+	var divCol = document.createElement("div");
+	divCol.className = "col-sm-10";
+	var input = document.createElement("input");
+	input.type = "text";
+	input.className = "form-control";
+	input.id = "step" + numSteps;
+	input.placeholder = "Step Details";
+	row.appendChild(label);
+	divCol.appendChild(input);
+	row.appendChild(divCol);
+	container.appendChild(row);
+}
