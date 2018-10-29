455ba26030c49badbe10bb236af7d3e99a367e96
Nasuru <37818222+Nasuru@users.noreply.github.com>
2018-10-29 16:55:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const http = require('http');
 const port=process.env.PORT || 3000
-const cheerio = require('cheerio');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-const $ = cheerio.load(fs.readFileSync(__dirname+"/index.html"));
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
