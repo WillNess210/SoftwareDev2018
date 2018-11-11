4bb0a542311157f10ad77e2023aabd1cfd116a17
WillNess210 <WillNess210@gmail.com>
2018-11-11 15:10:37 -0700

    testing navbar includes

--- index.php
+++ index.php
@@ -18,37 +18,7 @@
 
   <body>
 
-    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#">Title TBD</a>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pagetemplate.html">Explore</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-		<ul class="navbar-nav profile-button" >
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profile</a>
-				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown01">
-				  <a class="dropdown-item" href="login.html">Log In</a>
-				  <a class="dropdown-item disabled" href="#">Edit Profile</a>
-				  <a class="dropdown-item" href="upload.html">Upload a Recipe</a>
-				</div>
-          </li>
-		</ul>
-      </div>
-    </nav>
+    <?php include('predone/navbar.php'); ?>
 	
 	<!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
