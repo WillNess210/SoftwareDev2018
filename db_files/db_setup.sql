454a84223e3327057a483d0c788e42f4583de260
Robert Gomez <rgomez@cu-engr2-1-13-10.201.50.182.int.colorado.edu>
2018-11-12 17:26:59 -0700

    Deleted uncessary files

--- db_files/db_setup.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists users( id serial, username varchar(50) not null, email varchar(50) not null, primary key (id) );
-create table if not exists recipes( id serial, owner_id integer not null, name varchar(50) not null, category integer not null, public boolean not null, primary key(id) );
-create table if not exists steps( id serial, recipe_id integer not null, s_order integer not null, content text not null, primary key (id) );
-create table if not exists ingredients( id serial, recipe_id integer not null, name varchar(50) not null, amount varchar(50) not null, primary key (id) );
