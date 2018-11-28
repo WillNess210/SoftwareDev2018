0cdd148b6f72d1e835064cb504b0e2dc33c832a2
Harsh Deshpande <harshdeshpande@econ2-192-253-dhcp.int.colorado.edu>
2018-11-28 13:05:53 -0700

    Nothin

--- api.js
+++ api.js
@@ -91,7 +91,7 @@ app.get("/secret/page/", function(req, res) {
 
 app.get("/signin",function(req, res){
   if(!req.session.user)
-    res.sendFile("/login.html");
+    res.sendFile("login.html");
   else
     res.redirect("/dashboard");
 });
