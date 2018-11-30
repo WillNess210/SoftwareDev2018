caaa0182e4282dd7d5b2a76d2ba195e26128a802
WillNess210 <WillNess210@gmail.com>
2018-11-30 10:33:39 -0700

    resetting website

--- queries.js
+++ queries.js
@@ -23,6 +23,7 @@ const makeRecipe = function(req, res){
 		}
 		//res.status(200).send("Successfully added recipe: |" + id + "|" + owner_id + "|" + name + "|" + category + "|" + public + "|" + steps);
 	});
+	
 	//res.status(200).send('Successfully added recipe');
 };
 
