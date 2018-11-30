b37587ecd3ec0dfbc4fde2d93f9c3ea4b5e4964b
Harsh Deshpande <harshdeshpande@rgnt2-67-174-dhcp.int.colorado.edu>
2018-11-30 14:20:33 -0700

    edited variable

--- queries.js
+++ queries.js
@@ -175,7 +175,7 @@ const editUserEmail = function(req, res){
 const editUserPassword = function(req, res){
 	const {pwd} = req.body;
 	const editID = parseInt(req.params.id);
-	pool.query('update users set hash_pass = $1 where id = $2', [email, editID],function(err, results){
+	pool.query('update users set hash_pass = $1 where id = $2', [pwd, editID],function(err, results){
 		if (err) {
         	throw err;
       	}
