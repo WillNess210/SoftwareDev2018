ded53f417e3870f1ed70650bd91b5cd4a99aa94f
Harsh Deshpande <harshdeshpande@cu-engr2-2-1-10.201.66.90.int.colorado.edu>
2018-11-30 13:05:50 -0700

    Changed back post conditions

--- queries.js
+++ queries.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 
 
 const makeRecipe = function(req, res){
-	const {id, owner_id = 2, name, category = 5, public, steps} = req.body;
+	const {id, owner_id, name, category, public, steps} = req.body;
 	pool.query('insert into recipes values($1,$2,$3,$4,$5,$6)', [id, owner_id, name, category, public, steps], function(err, results){
 		if(err){
 			throw err;
@@ -24,7 +24,7 @@ const makeRecipe = function(req, res){
 		//res.status(200).send("Successfully added recipe: |" + id + "|" + owner_id + "|" + name + "|" + category + "|" + public + "|" + steps);
 	});
 	
-	//res.status(200).send('Successfully added recipe');
+	res.status(200).send('Successfully added recipe');
 };
 
 const getRecipe = function(req, res){
