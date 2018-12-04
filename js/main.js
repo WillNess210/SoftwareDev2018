ac46a34956d0492e67d16cab431ea95bf1e7be3f
Nasuru <37818222+Nasuru@users.noreply.github.com>
2018-12-03 18:06:02 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ function addIngredientField(){
 		inp1.className = "form-control";
 		inp1.placeholder = "Ingredient " + numIngredients + " Name";
 		inp1.id = "inp" + numIngredients + "name";
-		inp1.maxlength = 40;
+		inp1.maxlength = "40";
 		col1.appendChild(inp1);
 		var col2 = document.createElement("div");
 		col2.className = "col";
@@ -58,7 +58,7 @@ function addIngredientField(){
 		inp2.type = "text";
 		inp2.className = "form-control";
 		inp2.placeholder = "Ingredient Amount (ex: 1 cup, 8 handfuls, etc..)";
-		inp2.maxlength = 40;
+		inp2.maxlength = "40";
 		inp2.id = "inp" + numIngredients + "amt";
 		col2.appendChild(inp2);
 		row.appendChild(col1);
@@ -87,10 +87,16 @@ function addStepField(){
 		input.className = "form-control";
 		input.id = "step" + numSteps;
 		input.placeholder = "Step Details";
-		input.maxlength = 140;
+		input.maxlength = "140";
 		row.appendChild(label);
 		divCol.appendChild(input);
 		row.appendChild(divCol);
 		container.appendChild(row);
 	}
 }
+
+/*function removeIngredientField(){
+	if(numIngredients > 1){
+			
+	}
+}*/
