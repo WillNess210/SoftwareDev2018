73233814a14b12a4a6144fd9fc454a156dca235d
Robert Gomez <rgomez@Roberts-MacBook-Pro.local>
2018-12-09 16:57:59 -0700

    api test files

--- api.test.js
+++ api.test.js
@@ -1,5 +1,21 @@
-const sum = require('../apiTest');
+const recipeTest = require('../recipeTest');
+const ingredientTest = require('../ingredientTest');
+const userTest = require('../userTest');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+
+
+test('Api Call to Recipe of ID 410, should match info pulled directly from database', () => {
+	var data = recipeTest();
+	var id = data[0];
+	var owner = data[1];
+	var name - data[2];
+	var category = data[3];
+	var public = data[4];
+	var steps = data[5];
+  	expect(id.toBe(410);
+  	expect(id.toBe(17);
+  	expect(id.toBe("F in the Chat");
+  	expect(id.toBe(0);
+  	expect(id.toBe(true);
+  	expect(id.toBe("Press F in the Chat");
 });
--- apiTest.js
+++ apiTest.js
@@ -1,18 +1,18 @@
-function getRecipeTest(){
-	var url = "https://djibouti-recipe.herokuapp.com/api/recipes/40";
+exports.recipeTest = function getRecipeTest(){
+	var url = "https://djibouti-recipe.herokuapp.com/api/recipes/410";
 	$.getJSON(url, function( data ) {
 		return data;
 	});
 }
 
-function getUserTest(){
+exports.userTest = function getUserTest(){
 	var url = "https://djibouti-recipe.herokuapp.com/api/users/17";
 	$.getJSON(url, function( data ) {
 		return data;
 	});
 }
 
-function getIngredientTest(){
+exports.ingredientTest = function getIngredientTest(){
 	var url = "https://djibouti-recipe.herokuapp.com/api/ingredients/20";
 	$.getJSON(url, function( data ) {
 		return data;
