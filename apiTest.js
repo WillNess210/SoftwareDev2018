9c18be251af11b66475857d682501a45dc28ad88
WillNess210 <WillNess210@gmail.com>
2018-12-09 14:54:50 -0700

    added in test cases

--- apiTest.js
+++ apiTest.js
@@ -1,2 +1,20 @@
-var url = "https://djibouti-recipe.herokuapp.com/api/users/" + userId;
+function getRecipeTest(){
+	var url = "https://djibouti-recipe.herokuapp.com/api/recipes/40";
+	$.getJSON(url, function( data ) {
+		return data;
+	});
+}
 
+function getUserTest(){
+	var url = "https://djibouti-recipe.herokuapp.com/api/users/17";
+	$.getJSON(url, function( data ) {
+		return data;
+	});
+}
+
+function getIngredientTest(){
+	var url = "https://djibouti-recipe.herokuapp.com/api/ingredients/20";
+	$.getJSON(url, function( data ) {
+		return data;
+	});
+}
